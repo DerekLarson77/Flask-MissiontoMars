0d2b63e1fb3ba27a983c87cc05f79f9020f3481c
DerekLarson77 <DerekLarson77@gmail.com>
2021-06-15 17:06:05 -0700

    app flask file

--- Missions_to_Mars/app.py
+++ Missions_to_Mars/app.py
@@ -1,5 +1,6 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, render_template, redirect
 import pymongo
+from scrape_mars import scrape
 
 app = Flask(__name__)
 
@@ -7,13 +8,32 @@ app = Flask(__name__)
 conn = "mongodb://localhost:27017"
 client = pymongo.MongoClient(conn)
 
-# Select database and collection to use
+# connect to mongo db and collection
 db = client.scrape_data
-produce = db.produce
+mars_info = db.mars_info
+
+
+@app.route("/")
+def index():
+    # Find one record of data from the mongo database
+    mars_data = mars_info.find()
+
+    # Return template and data
+    return render_template("index.html", mars_data=mars_data)
+
 
 @app.route("/scrape")
 def scrape():
 
+    mars_data = scrape()
 
-@app.route("/")
-def home():
+
+    mars_info.insert_one(mars_data)
+
+    # Redirect back to home page
+    return redirect("/")
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
